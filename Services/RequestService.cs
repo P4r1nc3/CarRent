3e4a3d69a775813903f71bd88f18096ec04244d4
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-14 17:04:28 +0100

    Created CreateRequest method

--- Services/RequestService.cs
+++ Services/RequestService.cs
@@ -14,4 +14,35 @@ namespace CarRentApp.Services
         {
             _dbContext = dbContext;
         }
+
+        public void CreateRequest(int carId, int userId, DateTime startDate, DateTime endDate, bool isAccepted)
+        {
+            var car = _dbContext.Cars.FirstOrDefault(c => c.Id == carId);
+            if (car == null)
+            {
+                throw new KeyNotFoundException($"Car with ID {carId} not found.");
+            }
+
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                throw new KeyNotFoundException($"User with ID {userId} not found.");
+            }
+
+            var newRequest = new Request
+            {
+                CarId = carId,
+                UserId = userId,
+                Car = car,
+                User = user,
+                StartDate = startDate,
+                EndDate = endDate,
+                IsAccepted = isAccepted
+            };
+
+            _dbContext.Requests.Add(newRequest);
+            _dbContext.SaveChanges();
+        }
+
+    }
 }
