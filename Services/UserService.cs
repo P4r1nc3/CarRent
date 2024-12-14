0520717f13b7ce2643e9575892e6a1ce9d215259
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-14 17:15:22 +0100

    Created UserService

---
+++ Services/UserService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CarRentApp.Data;
+using CarRentApp.Models;
+
+namespace CarRentApp.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
