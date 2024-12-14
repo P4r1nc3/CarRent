7251af73a9423aa6c4d125eb25461d774e017548
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-14 15:20:48 +0100

    Created User model

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+}
