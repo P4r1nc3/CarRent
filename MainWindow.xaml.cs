95bb5efc41ffca3f6c3d5c88c67c4f4f50f4e06f
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-15 19:56:08 +0100

    Remade MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using CarRentApp.Context;
 using CarRentApp.Views.Login;
 using CarRentApp.Views.Register;
 using CarRentApp.Views.Employee;
@@ -7,23 +8,26 @@ namespace CarRentApp
 {
     public partial class MainWindow : Window
     {
+        private readonly LoginView _loginView;
+        private readonly RegisterView _registerView;
+
         public MainWindow()
         {
             InitializeComponent();
 
             // Initialize views
-            var loginView = new LoginView();
-            var registerView = new RegisterView();
-            var employeeView = new EmployeeView();
+            _loginView = new LoginView();
+            _registerView = new RegisterView();
 
             // Event handlers
-            loginView.SwitchToRegister += (_, __) => MainContent.Content = registerView;
-            loginView.SwitchToEmployee += (_, __) => MainContent.Content = employeeView;
-            registerView.SwitchToLogin += (_, __) => MainContent.Content = loginView;
-            employeeView.Logout += (_, __) => MainContent.Content = loginView;
+            _loginView.SwitchToRegister += (_, __) => MainContent.Content = _registerView;
+            _registerView.SwitchToLogin += (_, __) => MainContent.Content = _loginView;
+
+            _loginView.SwitchToEmployee += (_, __) => MainContent.Content = new EmployeeView();
+            // employeeView.Logout += (_, __) => MainContent.Content = loginView;
 
             // Set initial view
-            MainContent.Content = loginView;
+            MainContent.Content = _loginView;
         }
     }
 }
