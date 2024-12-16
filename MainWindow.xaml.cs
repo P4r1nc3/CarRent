1ef502dcb498ff52b5a50e401978a945b110603f
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-16 12:24:03 +0100

    Created sample admin view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,40 +4,48 @@ using CarRentApp.Views.Employee;
 using CarRentApp.Views.Login;
 using CarRentApp.Views.Register;
 using CarRentApp.Views.Mechanic;
+using CarRentApp.Views.Admin;
 
 namespace CarRentApp
 {
     public partial class MainWindow : Window
     {
-        private readonly LoginView _loginView;
         private readonly RegisterView _registerView;
+        private readonly LoginView _loginView;
+
+        private readonly AdminView _adminView;
+        private readonly MechanicView _mechanicView;
         private readonly EmployeeView _employeeView;
         private readonly CustomerView _customerView;
-        private readonly MechanicView _mechanicView;
 
         public MainWindow()
         {
             InitializeComponent();
 
             // Initialize views
-            _loginView = new LoginView();
             _registerView = new RegisterView();
+            _loginView = new LoginView();
+
+            _adminView = new AdminView();
+            _mechanicView = new MechanicView();
             _employeeView = new EmployeeView();
             _customerView = new CustomerView();
-            _mechanicView = new MechanicView();
+
 
             // Navigation Events
             _loginView.SwitchToRegister += (_, __) => MainContent.Content = _registerView;
             _registerView.SwitchToLogin += (_, __) => MainContent.Content = _loginView;
 
-            _loginView.SwitchToEmployee += (_, __) => MainContent.Content = _employeeView;
-            _loginView.SwitchToCustomer += (_, __) => MainContent.Content = _customerView;
+            _loginView.SwitchToAdmin += (_, __) => MainContent.Content = _adminView;
             _loginView.SwitchToMechanic += (_, __) => MainContent.Content = _mechanicView;
+            _loginView.SwitchToCustomer += (_, __) => MainContent.Content = _customerView;
+            _loginView.SwitchToEmployee += (_, __) => MainContent.Content = _employeeView;
 
             // Logout Events
-            _employeeView.Logout += (_, __) => MainContent.Content = _loginView;
-            _customerView.Logout += (_, __) => MainContent.Content = _loginView;
+            _adminView.Logout += (_, __) => MainContent.Content = _loginView;
             _mechanicView.Logout += (_, __) => MainContent.Content = _loginView;
+            _customerView.Logout += (_, __) => MainContent.Content = _loginView;
+            _employeeView.Logout += (_, __) => MainContent.Content = _loginView;
 
             // Set initial content
             MainContent.Content = _loginView;
