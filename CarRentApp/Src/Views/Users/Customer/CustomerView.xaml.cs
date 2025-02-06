6eeacb9d496e1d0f4b7309b91af66f3884d0a763
logger421 <wojteklotko3@gmail.com>
2025-02-06 13:50:43 -0800

    Added renting view logic where user can  create rent requests

--- CarRentApp/Src/Views/Users/Customer/CustomerView.xaml.cs
+++ CarRentApp/Src/Views/Users/Customer/CustomerView.xaml.cs
@@ -1,10 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using CarRentApp.Src.Contexts;
 using CarRentApp.Src.Models;
 using CarRentApp.Src.Repositories;
-using Microsoft.VisualBasic.Logging;
 
 namespace CarRentApp.Views.Users.Customer
 {
@@ -14,21 +12,28 @@ namespace CarRentApp.Views.Users.Customer
 
         private readonly AuthContext _authContext;
         private readonly RequestRepository _requestRepository;
+        private readonly CarRepository _carRepository;
+
+        private List<Car> _allCars = [];
         
         public CustomerView(DatabaseContext dbContext)
         {
             InitializeComponent();
-            
+
             _authContext = AuthContext.GetInstance();
             _authContext.CurrentUserChanged += LoadUserInfo;
             _authContext.CurrentUserChanged += LoadRequestListByUserId;
-            
+
             _requestRepository = new RequestRepository(dbContext);
+            _carRepository = new CarRepository(dbContext);
+
+            _allCars = _carRepository.GetCars();
             
             LoadUserInfo();
             LoadRequestListByUserId();
+            LoadCarList();
         }
-        
+
         private void Logout_Click(object sender, RoutedEventArgs e)
         {
             _authContext.Logout();
@@ -42,11 +47,56 @@ namespace CarRentApp.Views.Users.Customer
                 ? $"Logged in as: {currentUser.Name} {currentUser.Surname}"
                 : "No user is currently logged in.";
         }
-        
+
         private void LoadRequestListByUserId()
         {
             var currentUser = _authContext.GetCurrentUser();
-            RequestsDataGrid.ItemsSource = currentUser != null ? _requestRepository.GetRequestsByUserIdDescending(currentUser.Id) : [];
+            RequestsDataGrid.ItemsSource = currentUser != null
+                ? _requestRepository.GetRequestsByUserIdDescending(currentUser.Id)
+                : [];
+        }
+
+        private void LoadCarList()
+        {
+            CarDataGrid.ItemsSource = _allCars.Where(c => c.CarState == CarState.Available);
+        }
+        
+        private void RentRequest_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedRequestCar = CarDataGrid.SelectedItem as Car;
+            var currentUser = _authContext.GetCurrentUser();
+            
+            if (selectedRequestCar == null)
+            {
+                MessageBox.Show("Please select a car to rent.", "No Selection", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!IsOnlyOneCarRentedCheck())
+            {
+                MessageBox.Show("You can rent only one car at a time.", "Limit exceeded", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                _requestRepository.CreateRequest(selectedRequestCar.Id, currentUser!.Id, DateTime.Now, DateTime.Today.Add(TimeSpan.FromDays(2)), false);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+        
+        private bool IsOnlyOneCarRentedCheck()
+        {
+            var user = _authContext.GetCurrentUser();
+            
+            var userRequests = _requestRepository.GetRequestsByUserIdDescending(user!.Id);
+            var rentedCars = _allCars.Where(car => car.CarState is CarState.Rented or CarState.Reserved);
+
+            return userRequests.Exists(r => rentedCars.Contains(r.Car));
         }
     }
 }
